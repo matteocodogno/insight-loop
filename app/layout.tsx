bfbc9b48b000e139f2a62183756333ec1478fb02
Matteo Codogno <matteo.codogno@welld.ch>
2025-12-05 15:14:51 +0100

    feat: add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ThemeProvider } from "@/components/theme-provider";
 
 import "./globals.css";
@@ -80,6 +81,7 @@ export default function RootLayout({
         >
           <Toaster position="top-center" />
           <SessionProvider>{children}</SessionProvider>
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
