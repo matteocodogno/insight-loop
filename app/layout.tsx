4e310a67f3d696eab0409f399cb2be53be1a7012
Matteo Codogno <matteo.codogno@welld.ch>
2025-12-05 15:17:53 +0100

    feat: add web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { ThemeProvider } from "@/components/theme-provider";
 
 import "./globals.css";
@@ -82,6 +83,7 @@ export default function RootLayout({
           <Toaster position="top-center" />
           <SessionProvider>{children}</SessionProvider>
           <SpeedInsights />
+          <Analytics/>
         </ThemeProvider>
       </body>
     </html>
